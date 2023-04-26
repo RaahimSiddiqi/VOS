e5efc3da1d922be0c8053c11727cfa9ba81f2af0
Mahad095 <k200338@nu.edu.pk>
2023-04-27 02:46:36 +0500

    Added checkboxes for toggling masks, bb, confidence

--- View/src/components/Controller/index.tsx
+++ View/src/components/Controller/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Slider, Button, FormControl, InputLabel, Select, MenuItem, Typography, Grid, Box } from '@mui/material';
+import { Slider, Button, FormControl, InputLabel, Select, MenuItem, Typography, Grid, Box, FormGroup, FormControlLabel, Checkbox, FormHelperText } from '@mui/material';
 import Dropzone, { DropzoneRef, useDropzone } from 'react-dropzone'
 import ColorLensIcon from '@mui/icons-material/ColorLens';
 import ImageIcon from '@mui/icons-material/Image';
 import { MediaFilePreview, useMediaUpload } from '../../components';
-import { RgbaStringColorPicker  } from "react-colorful";
+import { RgbaStringColorPicker } from "react-colorful";
+import { useFormik } from "formik";
 
 interface ControllerProps {
   activeFile: File | undefined;
   onSubmit: (file: File) => any;
 }
+interface CheckboxGroupProps {
+  onSubmit: (values: any) => any
+  tags: string[]
+}
+
 
 const Controller: React.FC<ControllerProps> = ({ activeFile, onSubmit }) => {
   const [model, setModel] = React.useState('YOLOv8');
@@ -18,13 +24,24 @@ const Controller: React.FC<ControllerProps> = ({ activeFile, onSubmit }) => {
   const [iou, setiou] = React.useState(0.5);
   const [bgFile, setbgFile] = React.useState<File>();
   const [bgColor, setbgColor] = React.useState("rgba(0, 0, 0, 1)");
-  
+  const [checkboxGroup, setcheckboxGroup] = React.useState({
+    masks: true,
+    boxes: true,
+    conf: true
+  });
   const handleSegmentClick = async () => {
-    const params = { model, imageSize, confidence, iou};
+    const params = { model, imageSize, confidence, iou, checkboxGroup };
     //make request to server with params. server returns a file object
     const output_file = new File(["Meow Meow"], "Meow.txt") //sample file
     onSubmit(output_file);
   };
+  const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setcheckboxGroup({
+      ...checkboxGroup,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
   return (
     <form style={{ paddingRight: 30, paddingLeft: 30, paddingTop: 20, paddingBottom: 20 }}>
 
@@ -81,19 +98,19 @@ const Controller: React.FC<ControllerProps> = ({ activeFile, onSubmit }) => {
               "image/png": [".png"],
               "image/jpeg": [".jpg", ".jpeg"],
               "image/jpg": [".jpg", ".jpeg"],
-              }} 
-              onDrop = {acceptedFiles => acceptedFiles && setbgFile(acceptedFiles[0])}>
-              
-              {({getRootProps, getInputProps}) => (
-                <div {...getRootProps({style : {padding: 10, width : 200, height : 100, border : 'dashed gray 1px', borderRadius : '6%', display : 'flex', justifyContent : 'center', alignItems : 'center'}})}>
-                  <input {...getInputProps()}/>
-                  {!bgFile ? 
-                    <ImageIcon/>
+            }}
+              onDrop={acceptedFiles => acceptedFiles && setbgFile(acceptedFiles[0])}>
+
+              {({ getRootProps, getInputProps }) => (
+                <div {...getRootProps({ style: { padding: 10, width: 200, height: 100, border: 'dashed gray 1px', borderRadius: '6%', display: 'flex', justifyContent: 'center', alignItems: 'center' } })}>
+                  <input {...getInputProps()} />
+                  {!bgFile ?
+                    <ImageIcon />
                     :
-                    <MediaFilePreview file={bgFile}/>
+                    <MediaFilePreview file={bgFile} />
                   }
                 </div>
-              ) 
+              )
               }
             </Dropzone>
             {/* <background.Dropzone style = {{ width : 200, height : 100, border : 'dashed gray 1px', borderRadius : '6%'}}/> */}
@@ -103,6 +120,32 @@ const Controller: React.FC<ControllerProps> = ({ activeFile, onSubmit }) => {
             <RgbaStringColorPicker color={bgColor} onChange={setbgColor} />
           </li>
         </ul>
+      </Box>
+      <Box mt={5}>
+        <Typography variant="h6" color="initial">Advanced</Typography>
+        <FormControl sx={{ ml: 3 }} component='ul' variant="standard">
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox checked={checkboxGroup.masks} onChange={handleChange} name="masks" />
+              }
+              label="Display masks"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={checkboxGroup.boxes} onChange={handleChange} name="boxes" />
+              }
+              label="Display bounding boxes"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={checkboxGroup.conf} onChange={handleChange} name="conf" />
+              }
+              label="Display confidence"
+            />
+          </FormGroup>
+          <FormHelperText>Please select atleast one.</FormHelperText>
+        </FormControl>
       </Box>
     </form>
   );
