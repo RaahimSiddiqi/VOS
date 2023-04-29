f811b31538b3dae24a41bd8ff2734d0429189c89
Mahad095 <k200338@nu.edu.pk>
2023-04-30 00:09:00 +0500

    Merge

--- View/src/components/Editor/index.tsx
+++ View/src/components/Editor/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Typography, Box, Paper, CircularProgress, Backdrop } from '@mui/material';
 import { BackgroundController, InferenceParamsController, MediaUploadPreview } from './../../components';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
-import Dropzone, { DropzoneRef } from 'react-dropzone';
 import { Button } from '@mui/material';
 import { MediaUploadPreviewRef } from '../MediaUploadPreview';
 import ImageIcon from '@mui/icons-material/Image';
@@ -22,6 +20,8 @@ const download = (file: File) => {
 }
 
 const inference = async (video: File, inferenceParams: InferenceParamsInterface) => {
+  // const request = FormData
+  // return fetch()
   return new Promise<File>((resolve, reject) => {
     setTimeout(() => {
       fetch(rhino)
@@ -88,67 +88,68 @@ function Editor() {
           setmainFile(file);
           editorState.current = EditorState.Background;
         }));
-
+  const extractFetch = useAsyncCallback(
+    () => mainFile &&
+          background.current &&
+          backgroundChange(mainFile, background.current)
+          .then(file => setmainFile(file))
+  );
   return (
     <>
-    {isAuthenticated ?
-      <>
-        <Paper sx={{ mx: 'auto', padding: 5, my: 2 }} elevation={4}>
-          <Backdrop
-            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={segmentFetch.loading}
-          >
-            <CircularProgress color="inherit" />
-          </Backdrop>
-          {/* <Typography variant='h4' gutterBottom>Image/Video segmentation</Typography> */}
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
-              <Paper variant='outlined'>
-                <form style={{ paddingRight: 30, paddingLeft: 30, paddingTop: 20, paddingBottom: 20 }}>
-                  {
-                    editorState.current === EditorState.Inference ?
-                      <InferenceParamsController handleChange={newValue => inferenceParams.current = newValue} />
-                      :
-                      <BackgroundController handleChange={newValue => background.current = newValue} detectedClasses={['person', 'airplane']} />
-                  }
-                </form>
-                {/* <Controller activeFile={activeFile} onSubmit={(file) => console.log(file)} /> */}
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={8}>
-              <MediaUploadPreview
-                file={mainFile}
-                setfile={setmainFile}
-                ref={dropzoneRef}
-                accept={['video', 'image']}
-                uploadIcon={<ImageIcon fontSize='large' />}
-                uploadText={"Drag 'n Drop or Click to upload media"}
-                noClick={Boolean(mainFile)}
-                noDrag={Boolean(mainFile)}
-                style={{ width: '100%', height: '100%', border: 'dashed gray 1px', borderRadius: '4px' }}
-              />
-            </Grid>
+
+      <Paper sx={{ mx: 'auto', padding: 5, my: 2 }} elevation={4}>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={segmentFetch.loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+
+        <Typography variant='h4' gutterBottom>Image/Video segmentation</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Paper variant='outlined'>
+              <form style={{ paddingRight: 30, paddingLeft: 30, paddingTop: 20, paddingBottom: 20 }}>
+                {
+                  editorState.current === EditorState.Inference ?
+                    <InferenceParamsController handleChange={newValue => inferenceParams.current = newValue} />
+                    :
+                    <BackgroundController handleChange={newValue => background.current = newValue} detectedClasses={['person', 'airplane']} />
+                }
+              </form>
+              {/* <Controller activeFile={activeFile} onSubmit={(file) => console.log(file)} /> */}
+            </Paper>
           </Grid>
-          {mainFile &&
-            <>
-              <Box mt={3} display='flex' justifyContent='space-between'>
+          <Grid item xs={12} md={8}>
+            <MediaUploadPreview
+              file={mainFile}
+              setfile={setmainFile}
+              ref={dropzoneRef}
+              accept={['video', 'image']}
+              uploadIcon={<ImageIcon fontSize='large' />}
+              uploadText={"Drag 'n Drop or Click to upload media"}
+              noClick={Boolean(mainFile)}
+              noDrag={Boolean(mainFile)}
+              style={{ width: '100%', height: '100%', border: 'dashed gray 1px', borderRadius: '4px' }}
+            />
+          </Grid>
+        </Grid>
+        {mainFile &&
+          <>
+            <Box mt={3} display='flex' justifyContent='space-between'>
+              {editorState.current === EditorState.Inference ?            
                 <Button variant='contained' disabled={segmentFetch.loading} onClick={segmentFetch.execute}>Segment</Button>
-                <Box>
-                  <Button variant='outlined' sx={{ mr: 2 }} onClick={() => dropzoneRef.current?.openFileDialog()}>Change Media</Button>
-                  <Button variant='contained' onClick={() => download(mainFile)}>Save Media</Button>
-                </Box>
+                :
+                <Button variant='contained' disabled={segmentFetch.loading} onClick={segmentFetch.execute}>Extract</Button>
+              }
+              <Box>
+                <Button variant='outlined' sx={{ mr: 2 }} onClick={() => dropzoneRef.current?.openFileDialog()}>Change Media</Button>
+                <Button variant='contained' onClick={() => download(mainFile)}>Save Media</Button>
               </Box>
-            </>
-          }
-        </Paper>
-      </>
-      :
-      <Box sx={{display:"flex", width:"100%", height: 'calc(100vh - 65px)' }}>
-        <Box sx={{m:"auto"}}>
-          <Button variant="contained" onClick={() => navigate("/login")}>Please Login to Use</Button>
-        </Box>
-      </Box>
-    }
+            </Box>
+          </>
+        }
+      </Paper>
     </>
   )
 }
